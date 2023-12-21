7d69c118f9e57538bd5c65e0c15152349f32c18e
Viktor Marosvary <viktor.marosvary@gmail.com>
2023-12-21 22:18:02 +0100

    calendar style and code changes

--- calendar/lib_js/calendar.js
+++ calendar/lib_js/calendar.js
@@ -4,11 +4,8 @@ Calendar = new Object();
 // unix timestamp should be in seconds example 1357038000 01/01/2013
 // in JS unix stamps are in milliseconds so the one from php has to be multiplied by 1000
 Calendar.printRange = function (startDateUnix, endDateUnix) {
-
     var oContainer = document.getElementById("calendar-container");
-
     oContainer.innerHTML = "";
-
 
     var month = 60 * 60 * 24 * 30
 
@@ -19,17 +16,13 @@ Calendar.printRange = function (startDateUnix, endDateUnix) {
     var endDate = new Date(endDateUnix * 1000);
     var actualDate = new Date(startDateUnix * 1000);
 
-
     oContainer.appendChild(Calendar.printMonth(actualDate.getFullYear(), actualDate.getMonth() + 1));
 
     while (!(actualDate.getFullYear() == endDate.getFullYear() && actualDate.getMonth() == endDate.getMonth())) {
         actualDate.setMonth(actualDate.getMonth() + 1);
         oContainer.appendChild(Calendar.printMonth(actualDate.getFullYear(), actualDate.getMonth() + 1));
-        //alert("Actual Month:"+actualDate.getMonth()+" End Month:"+endDate.getMonth());
     }
-
 }
-
 
 Calendar.addEventList = function () {
     json = '({"items":[' +
@@ -45,19 +38,14 @@ Calendar.addEventList = function () {
     Elem = dataContent.items;
 
     var length = Elem.length;
-    var domElements = new Array();
     for (i = 0; i < length; i++) {
         Calendar.addEvent(Elem[i].date, Elem[i].title);
     }
 }
 
-
-
 Calendar.addEvent = function (date, title) {
-
     var oDate = new Date(date * 1000);
     var timeText = oDate.getHours() + ":" + oDate.getMinutes();
-
 
     var dateID = 'c_day_' + oDate.getFullYear() + '_' + (oDate.getMonth() + 1) + '_' + oDate.getDate();
     var oDay = document.getElementById(dateID);
@@ -70,16 +58,14 @@ Calendar.addEvent = function (date, title) {
 // month input is the number of the month for example 1-january , 2-february etc ; (2013,1) is January 2013
 Calendar.printMonth = function (year, month) {
     month--; // to get the proper month. By default 
-    var one_day = 1000 * 60 * 60 * 24;
-    var firstDay = new Date(year, month, 1, 12, 00, 00);
+    // var one_day = 1000 * 60 * 60 * 24;
+    var firstDay = new Date(year, month, 1, 12, 0, 0);
     var today = new Date();
+    var firstWeek = true;
     var day = firstDay.getDay();
     var dayFullNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
     var dayShortNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
     var monthFullNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    var rowCounter = 0;
-    var cellCounter = 0;
 
     if (day == 0) day = 7;
 
@@ -87,12 +73,9 @@ Calendar.printMonth = function (year, month) {
 
     var oTable = document.createElement("table");
     oTable.className = "c_month";
-    oTable.cellSpacing = 0;
-    oTable.cellPadding = 0;
-
 
     // Setting up the day headings 
-    var oRow = oTable.insertRow(-1);
+    let oRow = oTable.insertRow(-1);
     oRow.className = "c_columnHeading";
 
     for (var i = 0; i < 7; i++) {
@@ -101,41 +84,35 @@ Calendar.printMonth = function (year, month) {
         oAnchor.innerHTML = dayShortNames[i];
         oAnchor.href = "#";
         oHeading.appendChild(oAnchor);
-
     }
-
-
-
 
     // printing the days before the month
     while (actualDay.getMonth() != month) {
-
-        if (actualDay.getDay() == 1)
-            var oRow = oTable.insertRow(-1);
-        oRow.className = "c_week";
-
+        if (actualDay.getDay() == 1) {
+            oRow = oTable.insertRow(-1);
+            oRow.className = firstWeek ? "c_week c_first_week" : "c_week";
+        }
+        firstWeek = false;
         var oCell = oRow.insertCell(-1);
         oCell.className = "c_" + dayFullNames[actualDay.getDay()];
         var oAnchor = document.createElement("a");
         oAnchor.className = "c_dayOtherMonth";
         oAnchor.href = "#";
         oAnchor.innerHTML = actualDay.getDate();
-
         oCell.appendChild(oAnchor);
 
         // adding a day
         actualDay.setDate(actualDay.getDate() + 1);
-
-
     }
 
     // printing the actual month
     while (actualDay.getMonth() == month) {
         var elementIdName = 'c_day_' + actualDay.getFullYear() + '_' + (actualDay.getMonth() + 1) + '_' + actualDay.getDate();
-        if (actualDay.getDay() == 1)
-            var oRow = oTable.insertRow(-1);
-        oRow.className = "c_week";
-
+        if (actualDay.getDay() == 1) {
+            oRow = oTable.insertRow(-1);
+            oRow.className = firstWeek ? "c_week c_first_week" : "c_week";
+        }
+        firstWeek = false;
         var oCell = oRow.insertCell(-1);
         oCell.className = "c_" + dayFullNames[actualDay.getDay()];
         var oAnchor = document.createElement("a");
@@ -150,15 +127,15 @@ Calendar.printMonth = function (year, month) {
         oCell.appendChild(oAnchor);
         // adding a day
         actualDay.setDate(actualDay.getDate() + 1);
-
     }
 
     // printing the days after the month
     while (actualDay.getDay() != 1) {
-
-        if (actualDay.getDay() == 1)
-            var oRow = oTable.insertRow(-1);
-        oRow.className = "c_week";
+        if (actualDay.getDay() == 1) {
+            oRow = oTable.insertRow(-1);
+            oRow.className = firstWeek ? "c_week c_first_week" : "c_week";
+        }
+        firstWeek = false;
 
         var oCell = oRow.insertCell(-1);
         oCell.className = "c_" + dayFullNames[actualDay.getDay()];
@@ -168,14 +145,11 @@ Calendar.printMonth = function (year, month) {
         oAnchor.innerHTML = actualDay.getDate();
 
         oCell.appendChild(oAnchor);
-
-
         // adding a day
         actualDay.setDate(actualDay.getDate() + 1);
-
     }
 
-
+    oRow.className = "c_week c_last_week";
 
     var oYear = document.createElement("a");
     oYear.href = "#";
@@ -190,20 +164,12 @@ Calendar.printMonth = function (year, month) {
     var oContainer = document.createElement("div");
     oContainer.className = "calendarContainer";
 
-
     oContainer.appendChild(oMonth);
     oContainer.appendChild(oYear);
     oContainer.appendChild(oTable);
 
     return oContainer;
-
 }
-
-
-
-
-
-
 
 
 // adds a day to the date object received. 
